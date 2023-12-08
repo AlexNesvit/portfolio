08c93a590009ea7000f5829d128ed838c4d7d51f
Afpa <alex.nesvit2000@gmail.com>
2023-12-08 10:03:59 +0100

    js toggle icon navbar

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 //scroll sections
 let section = document.querySelectorAll('section');
@@ -10,9 +16,9 @@ window.onscroll = () => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute('id')
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height ) {
+        if(top >= offset && top < offset + height) {
            // active navbar links
            navLinks.forEach(links => {
             links.classList.remove('active');
