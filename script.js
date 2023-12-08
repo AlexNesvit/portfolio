8b2a7297f351f4e3a7305596a892702dfd8cc168
Afpa <alex.nesvit2000@gmail.com>
2023-12-08 08:59:57 +0100

    js active navbar links

--- script.js
+++ script.js
@@ -2,7 +2,24 @@
 
 
 //scroll sections
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height ) {
+           // active navbar links
+           navLinks.forEach(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+           });
+        }
+    });
     // sticky header
     let header = document.querySelector('header');
 
